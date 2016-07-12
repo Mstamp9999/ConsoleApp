4e7e2aa2a22a0fc1d6ed7c0af4a697c70a1a4aae
Martin Stamper <mstamp9999@my.sctd.edu>
2016-07-12 19:50:19 -0400

    update
    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,33 +6,86 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    public class User
+    {
+        public string Name;
+        public string UserName;
+        public string Password;
+        public string Age;
+        public string PhoneNumber;
+        public string Birthday;
+        public string Email;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            string user_input = "";
-            string user_tag = "";
-            string user_pass = "";
-            string user_age = "";
-            string user_bdate = "";
+            User user_object = new User();
+
             Console.WriteLine("Hello World");
             Console.WriteLine("What is your name ");
-            user_input = Console.ReadLine();
+            user_object.Name = Console.ReadLine();
             Console.WriteLine("What is your username ");
-            user_tag = Console.ReadLine();
+            user_object.UserName = Console.ReadLine();
             Console.WriteLine("What is your password ");
-            user_pass = Console.ReadLine();
+            user_object.Password = Console.ReadLine();
             Console.WriteLine("What is your age ");
-            user_age = Console.ReadLine();
+            user_object.Age = Console.ReadLine();
             Console.WriteLine("What is your birthday ");
-            user_bdate = Console.ReadLine();
-            
-            Console.WriteLine("Hello " + user_input);
-            Console.WriteLine("Your username is " + user_tag);
-            Console.WriteLine("Your password is " + user_pass);
-            Console.WriteLine("Your age is " + user_age + " Years old");
-            Console.WriteLine("Your birthday is " + user_bdate);
+            user_object.Birthday = Console.ReadLine();
+            Console.WriteLine("What is your Email ");
+            user_object.Email = Console.ReadLine();
+            Console.WriteLine("What is your Phone Number ");
+            user_object.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Hello " + user_object.Name);
+            Console.WriteLine("Your username is " + user_object.UserName);
+            Console.WriteLine("Your password is " + user_object.Password);
+            Console.WriteLine("Your are " + user_object.Age + " Years old");
+            Console.WriteLine("Your birthday is " + user_object.Birthday);
+            Console.WriteLine("Your Email is " + user_object.Email);
+            Console.WriteLine("Your PhoneNumber is " + user_object.PhoneNumber);
             Console.ReadLine();
+        }
+    }
+
+    class WriteTextFile
+    {
+        static void writeLine()
+        {
+
+
+            string[] lines = { "Name", "UserName", "Password", "Age", "Birthday", "Email", "PhoneNumber" };
+            System.IO.File.WriteAllLines(@"E:\WriteLines.txt", lines);
+
+            string text = "A class is the most powerful data type in C#. Like a structure, " +
+                           "a class defines the data and behavior of the data type. ";
+            System.IO.File.WriteAllText(@"E:\WriteText.txt", text);
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"E:\WriteLines2.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    if (!line.Contains("Name"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"E:\WriteText2.txt", true))
+            {
+                file.WriteLine("Name");
+                file.WriteLine("UserName");
+                file.WriteLine("Password");
+                file.WriteLine("Age");
+                file.WriteLine("Birthday");
+                file.WriteLine("Email");
+                file.WriteLine("PhoneNumber");
+            }
         }
     }
 }
